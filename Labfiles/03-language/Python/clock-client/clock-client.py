23cae08a3668d49d40dff7c4a74623de11779291
Gonzalo Antonio Sosa Malaga <gonzaloantonio@gmail.com>
2023-12-23 05:09:56 +0000

    Understanding model example

--- Labfiles/03-language/Python/clock-client/clock-client.py
+++ Labfiles/03-language/Python/clock-client/clock-client.py
@@ -5,7 +5,8 @@ from datetime import datetime, timedelta, date
 from dateutil.parser import parse as is_date
 
 # Import namespaces
-
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.language.conversations import ConversationAnalysisClient
 
 def main():
 
@@ -22,14 +23,76 @@ def main():
             if userText.lower() != 'quit':
 
                 # Create a client for the Language service model
-
+                client = ConversationAnalysisClient (ls_prediction_endpoint, AzureKeyCredential (ls_prediction_key))
                 # Call the Language service model to get intent and entities
+                cls_project = 'Clock'
+                deployment_slot = 'production'
+
+                with client:
+                    query = userText
+                    result = client.analyze_conversation (
+                        task = {
+                            "kind": "Conversation",
+                            "analysisInput": {
+                                "conversationItem": {
+                                    "participantId": "1",
+                                    "id": "1",
+                                    "modality": "text",
+                                    "language": "en",
+                                    "text": query
+                                },
+                                "isLoggingEnabled": False
+                            },
+                            "parameters": {
+                                "projectName": cls_project,
+                                "deploymentName": deployment_slot,
+                                "verbose": True
+                            }
+                        }
+                    )
+                
+                top_intent = result["result"]["prediction"]["topIntent"]
+                entities = result["result"]["prediction"]["entities"]
+
+                print ("\nView top intent:")
+                print ("\ttop intent: {}" . format (result["result"]["prediction"]["topIntent"]))
+                print ("\tcategory: {}" . format (result["result"]["prediction"]["intents"][0]["category"]))
+                print ("\tconfidence score: {}\n" . format (result["result"]["prediction"]["intents"][0]["confidenceScore"]))
+
+                print ("view entities:")
+                for entity in entities:
+                    print ("\tcategory {}" . format (entity["category"]))
+                    print ("\ttext {}" . format (entity["text"]))
+                    print ("\tconfidence score: {}" . format(entity["confidenceScore"]))
+
+                print ("query: {}" . format(result["result"]["query"]))
 
                 # Apply the appropriate action
-
+                if top_intent == 'GetTime':
+                    location = 'local'
+                    if len(entities) > 0:
+                        for entity in entities:
+                            if 'Location' == entity["category"]:
+                                location = entity["text"]
+                    print (GetTime(location))
+                elif top_intent == 'GetDay':
+                    date_string = date.today().strftime("%m/%d/%Y")
+                    if len(entities) > 0:
+                        for entity in entities:
+                            if 'Date' == entity["category"]:
+                                date_string = entity["text"]
+                    print (GetDay(date_string))
+                elif top_intent == 'GetDate':
+                    day = 'today'
+                    if len(entities) > 0:
+                        for entity in entities:
+                            if 'Weekday' == entity["category"]:
+                                day = entity["text"]
+                    print (GetDate(day))
+                else:
+                    print ("Try asking me for the time, the day, or the date.")
     except Exception as ex:
         print(ex)
-
 
 def GetTime(location):
     time_string = ''
